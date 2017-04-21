419c6a478e8670c7ebf161209a1e25f8329eed6d
BrandonNolte <brandon.nolte@gmail.com>
2017-04-21 15:39:00 -0700

    Initial checkin.

---
+++ listdir.py
@@ -0,0 +1,12 @@
+import os
+
+#  Open file and read it in line by line
+with open('/Users/bnolte/listdir/list.txt') as f:
+  directories = f.readlines()
+
+# For each line (directory) list the contents of the dir
+for subdir in directories:
+  dircontents =  os.listdir(subdir.rstrip())
+  print(subdir)
+  print(dircontents)
+
