a9d0f255bc1d04dd759d68b192786b98b37099ed
BrandonNolte <brandon.nolte@gmail.com>
2017-04-23 14:03:56 -0700

    added some print statments to help with debugging. I have also fixed some functions

--- listdir.py
+++ listdir.py
@@ -6,19 +6,6 @@ from bson import BSON
 from bson import json_util
 from termcolor import colored
 
-def mongodb_connect(client_uri):
-    try:
-        return pymongo.MongoClient(client_uri)
-    except pymongo.errors.connectionFailure:
-         print "I have failed to connect to mongo {}".format(client_uri)
-
-
-def insert_into_database(self):
-    client_uri = "mongodb://localhost:27017"
-    client = mongodb_connect(client_uri)
-    db = client['mongodirectorylist']
-
-
 #  Open file and read it in line by line
 with open('/Users/bnolte/listdir/list.txt') as f:
   directories = f.readlines()
@@ -30,6 +17,23 @@ for subdir in directories:
   dircontents_parsed = json.dumps(dircontents)
 #  print(dircontents_parsed)
 
+def mongodb_connect(client_uri):
+    try:
+        return pymongo.MongoClient(client_uri)
+    except pymongo.errors.connectionFailure:
+         print "I have failed to connect to mongo {}".format(client_uri)
+    return
 
-print (mongodb_connect)
-insert_into_database["mongodirectorylist"].insert(dircontents_parsed)
+def connect_to_database(IsThisAnParamButIsThisAlsoAnObject):  #this is my keyword def
+    client_uri = "mongodb://localhost:27017"
+    client = mongodb_connect(client_uri)
+    db = client['mongodirectorylist']
+    print client #this should let me see if client and client uri are being called
+    return
+
+
+# IsThisAnParamButIsThisAlsoAnObject = (dircontents_parsed) #do i put my insert here? I want to insert dircontents parsed via json to mongo
+
+mongodb_connect("mongodb://localhost:27017")
+print connect_to_database(IsThisAnParamButIsThisAlsoAnObject = ("")) #are functioncalls required to have parameters fed into them? 
+#connect_to_database().insert(dircontents_parsed)
