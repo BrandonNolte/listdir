9cd63a8e3dae625107b3f0a013538d5114328b2b
BrandonNolte <brandon.nolte@gmail.com>
2017-04-22 19:35:25 -0700

    Set directories to red

--- listdir.py
+++ listdir.py
@@ -2,8 +2,9 @@ import os
 import pprint
 from termcolor import colored
 
+
 pp = pprint.PrettyPrinter(indent=4)
-ppfiles = pprint.PrettyPrinter(indent=4)
+#ppfiles = pprint.PrettyPrinter(indent=4)
 
 #  Open file and read it in line by line
 with open('/Users/bnolte/listdir/list.txt') as f:
@@ -12,6 +13,6 @@ with open('/Users/bnolte/listdir/list.txt') as f:
 # For each line (directory) list the contents of the dir
 for subdir in directories:
   dircontents =  os.listdir(subdir.rstrip())
-  print colored(subdir)
+  print colored(subdir, 'red')
   pp.pprint(dircontents)
 
