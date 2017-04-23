502af3b74877445288e0cabf3099eb5c55002f1f
BrandonNolte <brandon.nolte@gmail.com>
2017-04-22 22:53:15 -0700

    Midpoint in databaseviajson

--- listdir.py
+++ listdir.py
@@ -1,9 +1,23 @@
 import os
 import pprint
+import json
+import pymongo
+from bson import BSON
+from bson import json_util
 from termcolor import colored
 
+def mongodb_connect(client_uri):
+    try:
+        return pymongo.MongoClient(client_uri)
+    except pymongo.errors.connectionFailure:
+         print "I have failed to connect to mongo {}".format(client_uri)
 
-pp = pprint.PrettyPrinter(indent=4)
+
+def insert_into_database(self):
+    client_uri = "mongodb://localhost:27017"
+    client = mongodb_connect(client_uri)
+    db = client['mongodirectorylist']
+
 
 #  Open file and read it in line by line
 with open('/Users/bnolte/listdir/list.txt') as f:
@@ -12,6 +26,11 @@ with open('/Users/bnolte/listdir/list.txt') as f:
 # For each line (directory) list the contents of the dir
 for subdir in directories:
   dircontents =  os.listdir(subdir.rstrip())
-  print colored(subdir, 'red')
-  pp.pprint(dircontents)
+#  print colored(subdir, 'red')
+  dircontents_parsed = json.dumps(dircontents)
+#  print(dircontents_parsed)
 
+
+print (mongodb_connect)
+#if connection is not None:
+insert_into_database["mongodirectorylist"].insert(dircontents_parsed)
