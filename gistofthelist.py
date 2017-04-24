77da3e7ed0ac85180687c4ac2a0fb0d16fca1112
BrandonNolte <brandon.nolte@gmail.com>
2017-04-24 13:38:11 -0700

    Adding debug

--- gistofthelist.py
+++ gistofthelist.py
@@ -1,11 +1,12 @@
 import os
+import sys
 import pprint
 import json
 import pymongo
 from pymongo import MongoClient
 from bson import BSON
 from bson import json_util
-from termcolor import colored
+
 client_uri = "mongodb://localhost:27017"
 client = pymongo.MongoClient(client_uri)
 db = client['mongodirectorylist']
@@ -17,16 +18,27 @@ collection = client.mongodirectorylist.mongocollection
 with open('/Users/bnolte/listdir/list.txt') as f:
   directories = f.readlines()
 
-for files in directories:
-  dircontents =  os.listdir(files.rstrip())
+for x in directories:
+  filelist =  os.listdir(x.rstrip())
+  formattedfiles = {"File": (filelist)}
+
+sys.exit()  
 
 
-json_string = json.dumps(dircontents)
 
-print json_string
-print collection
-print client.mongodirectorylist
+#print formattedfiles
 
+post = collection.insert_one(formattedfiles)
+#print collection.find()
+print post.inserted_id
+pprint.pprint(collection.find_one({"_id": post.inserted_id})) 
+
+#collectioninsert = collection.(json_string)
+
+
+
+
+#print collection
 
 
 #need to get dircontents into map format
