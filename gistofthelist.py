458fd581619482a56e9917ee7e3c89cfac200157
BrandonNolte <brandon.nolte@gmail.com>
2017-04-24 12:11:50 -0700

    Converted directories to json and print that.

--- gistofthelist.py
+++ gistofthelist.py
@@ -9,16 +9,25 @@ from termcolor import colored
 client_uri = "mongodb://localhost:27017"
 client = pymongo.MongoClient(client_uri)
 db = client['mongodirectorylist']
-client.mongodirectorylist
+#client.mongodirectorylist
+pp = pprint.PrettyPrinter(indent=4)
+collection = client.mongodirectorylist.mongocollection
+
 
 with open('/Users/bnolte/listdir/list.txt') as f:
   directories = f.readlines()
 
-for subdir in directories:
-  dircontents =  os.listdir(subdir.rstrip())
-  dircontents_parsed = json.dumps(dircontents)
+for files in directories:
+  dircontents =  os.listdir(files.rstrip())
+
+
+json_string = json.dumps(dircontents)
+
+print json_string
+print collection
+print client.mongodirectorylist
+
 
 
 #need to get dircontents into map format
-
-result = client.mongodirectorylist.mongocollection.insert_many(dircontents)
+#result = client.mongodirectorylist.mongocollection.insert_many(dircontents)
